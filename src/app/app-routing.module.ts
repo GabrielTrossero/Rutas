8746c605d16b7c7b5044778b27b8cb57e06aa394
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-11 14:52:57 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { InfoComponent } from './componentes/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'informacion', component: InfoComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: 'contacto2', component: ContactComponent}, //podemos reutilizar el componente 
+  {path: 'sobre', component: AboutComponent},
+  {path: 'about', redirectTo: 'sobre'}, //o podemos redirigir a otro componente
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
