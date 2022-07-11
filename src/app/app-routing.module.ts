ffe4b21acf6c8e17f7b253939b109756ff1f0d75
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-11 15:19:48 -0300

    ruta raiz y ruta **

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ContactComponent } from './componentes/contact/contact.component';
 import { InfoComponent } from './componentes/info/info.component';
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'informacion'}, //ruta raiz 
   {path: 'informacion', component: InfoComponent},
   {path: 'contacto', component: ContactComponent},
   {path: 'contacto2', component: ContactComponent}, //podemos reutilizar el componente 
   {path: 'sobre', component: AboutComponent},
-  {path: 'about', redirectTo: 'sobre'}, //o podemos redirigir a otro componente
+  {path: 'about', redirectTo: 'sobre'}, //podemos redirigir a otro componente
+  {path: "**", redirectTo: 'contacto'}, //ruta por defecto para cuando ponemos una ruta "mal"
 ];
 
 @NgModule({
